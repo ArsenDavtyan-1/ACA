d945c9cae68d08cb62522c9bcd5e76dc8e7ea118
ArsenDavtyan-1 <arsend704@gmail.com>
2024-06-06 22:57:38 +0400

    my containers

--- myVector,myList/main.cpp
+++ myVector,myList/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "myList.h"
 #include "myvector.h"
+#include "myDoubleList.h"
 #include <initializer_list>
 
 int main()
@@ -32,40 +33,75 @@ int main()
 
 	//my::vector<int> C = { 4,0,5,-9,2,-99,45,12,84,54,12,5,4,7,-55,9,-9,1,54,0,3,55,8 };
 
-	my::vector<int> A;
-	A.push_back(7);
-	A.print();
-	my::List<int> L;
-	L.push_front(66);
-	L.push_front(-7);
-	L.push_front(0);
-	L.push_front(51);
-	L.print();
-	L.pop_front();
-	L.insert(11, 3);
-	L.insert(1, 5);
-	L.insert(-7, 4);
-	L.push_front(88);
-	L.push_front(-6);
-	L.push_front(-744);
-	L.push_front(171);
-	L.push_front(501);
-	L.print();
+	//my::vector<int> A;
+	//A.push_back(7);
+	//A.print();
+	//my::List<int> L;
+	//L.push_front(66);
+	//L.push_front(-7);
+	//L.push_front(0);
+	//L.push_front(51);
+	//L.print();
+	//L.pop_front();
+	//L.insert(11, 3);
+	//L.insert(1, 5);
+	//L.insert(-7, 4);
+	//L.push_front(88);
+	//L.push_front(-6);
+	//L.push_front(-744);
+	//L.push_front(171);
+	//L.push_front(501);
+	//L.print();
 
-	L.reverse();
-	L.print();
-	L.last_Nth(5);
-	L.push_front(-54);
-	L.push_front(881);
-	L.push_front(3);
-	L.print();
-	L.last_Nth(5);
-	L.last_Nth(0);
-	L.last_Nth(11);
-	L.last_Nth(22);
-	L.last_Nth(12);
-	L.last_Nth(13);
-	L.print();
-	L.reverse();
-	L.print();
+	//L.reverse();
+	//L.print();
+	//L.last_Nth(5);
+	//L.push_front(-54);
+	//L.push_front(881);
+	//L.push_front(3);
+	//L.print();
+	//L.last_Nth(5);
+	//L.last_Nth(0);
+	//L.last_Nth(11);
+	//L.last_Nth(22);
+	//L.last_Nth(12);
+	//L.last_Nth(13);
+	//L.print();
+	//L.reverse();
+	//L.print();
+
+
+	my::doubleList<int> D;
+	for (int i = 0; i < 5; ++i)
+	{
+		D.push_front(rand() % 20);
+	}
+	D.print();
+	D.pop_front();
+	D.print();
+	D.pop_front();
+	D.print();
+	D.pop_front();
+	D.pop_front();
+	D.print();
+	D.pop_front();
+	D.print();
+	D.pop_front();
+	D.push_back(12);
+	D.push_front(11);
+	D.push_back(-7);
+	D.push_back(-91);
+	D.push_front(0);
+	D.print();
+	D.pop_back();
+	D.pop_front();
+	D.print();
+	D.pop_back();
+	D.print();
+	D.pop_back();
+	D.print();
+	D.pop_back();
+	D.print();
+	D.pop_back();
+	D.print();
 }
